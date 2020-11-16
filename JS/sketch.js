921f62852d3ca2d1405659dc274da6c8f47f5550
KyraLNBoyle <kyra.boyle@students.plymouth.ac.uk>
2020-11-16 01:22:46 +0000

    Added a pop up instead of a new page for instructions

--- JS/sketch.js
+++ JS/sketch.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
    $('#FaceLogo').delay(4000).fadeIn();
    $('#EraseLogo').delay(6000).fadeIn();
 });
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
