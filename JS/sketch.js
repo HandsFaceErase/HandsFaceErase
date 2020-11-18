e2329563c7a91335f3cf7e132f4f27cb6d82b160
Jake Warren <33846480+JakeDWarren@users.noreply.github.com>
2020-11-18 12:20:52 +0000

    Added js for ar game

--- JS/sketch.js
+++ JS/sketch.js
@@ -2,6 +2,7 @@ $(document).ready( function() {
    $("#HandsLogo").delay(2000).fadeIn();
    $("#FaceLogo").delay(4000).fadeIn();
    $("#EraseLogo").delay(6000).fadeIn();
+   $("#PlayNow").delay(6000).fadeIn();
 });
 
 $(document).ready(function(){
