672d918f24839c36bfa7f66fa45475cdd17e40ce
KyraLNBoyle <kyra.boyle@students.plymouth.ac.uk>
2020-12-01 22:46:25 +0000

    adding leaderboard, geo location adn health bar

--- JS/sketch.js
+++ JS/sketch.js
@@ -35,3 +35,29 @@ $(document).ready(function() {
         $("#status").text("Status: Playing");
     });
 });
+
+// Health bar
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+  var amt = 100;
+
+  function attack(event){
+    $('.hit').on('click', function(){
+      amt = amt - 10;
+      $('.healthBarValue').css('width', amt + '%' )
+    });
+  }
+
+  attack();
+
+  function fullHealth(event){
+    $('.reset').on('click', function(){
+      amt = 100;
+      $('.healthBarValue').css('width', '100%');
+    })
+  }
+
+  fullHealth();
+
+});
